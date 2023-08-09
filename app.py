aa13c203d2122939ad37e7239e8d699103d3e5f0
laurentia25 <lvasilasco25@gmail.com>
2023-08-09 12:52:16 +0300

    second commit

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+Entrypoint-ul in aplicatia pentru proiectul final
+dezvoltata folosind framework-ul Flask
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7000)
