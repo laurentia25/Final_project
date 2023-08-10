c3cb37123fe2a850d6954ae371e9f5c3b4498d01
laurentia25 <lvasilasco25@gmail.com>
2023-08-10 21:19:12 +0300

    database implementation + login page

--- app.py
+++ app.py
@@ -3,14 +3,28 @@ Entrypoint-ul in aplicatia pentru proiectul final
 dezvoltata folosind framework-ul Flask
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from db.db_connection import create_database
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    # logica asociata cu metoda POST, nu e nevoie de else
+    user_data = dict(request.form) #-> dictionar cu info din form
+    print("===================")
+    print(user_data)
+
+
 if __name__ == '__main__':
+    create_database()
     app.run(debug=True, port=7000)
---
+++ db/db_connection.py
@@ -0,0 +1,66 @@
+"""
+Setarea functionalitatilor pentru crearea bazei de date si a tabelelor
+"""
+import sqlite3
+DB_PATH = "LebiZoux.db"
+
+
+# Stabilirea conexiunii cu baza de date
+def get_db_connection(db_path=DB_PATH):
+    connection = sqlite3.connect(db_path)
+    return connection
+
+
+# Crearea bazei de date si a tabelelor
+def create_database(db_path=DB_PATH):
+    # creare baza de date
+    connection = sqlite3.connect(db_path)
+    # creare tabele
+    create_tables(connection)
+
+
+def create_tables(connection):
+    create_users_table(connection)
+    create_products_table(connection)
+    create_orders_table(connection)
+    create_order_items_table(connection)
+
+
+def create_users_table(connection):
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Users(
+        id TEXT NOT NULL PRIMARY KEY,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        email TEXT NOT NULL        
+        );
+        """
+    )
+    connection.commit()
+
+
+def create_products_table(connection):
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS Products(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        description TEXT NOT NULL,
+        available_quantity INTEGER NOT NULL DEFAULT 0,
+        image TEXT        
+        );
+        """
+    )
+    connection.commit()
+
+
+def create_orders_table(connection):
+    pass
+
+
+def create_order_items_table(connection):
+    pass
