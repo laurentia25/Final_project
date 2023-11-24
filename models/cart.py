719c6349c80362e1b6f64975dc1ce8cb81caa86c
laurentia25 <lvasilasco25@gmail.com>
2023-11-24 21:34:12 +0200

    Cart implementation

---
+++ models/cart.py
@@ -0,0 +1,17 @@
+from db.CRUD.products_crud import ProductsDb
+
+
+class Cart:
+
+    def __init__(self, products_to_cart, product_to_buy):
+
+        self.products_to_cart = products_to_cart
+        self.product_to_buy = product_to_buy
+        self.products_db = ProductsDb()
+
+    def add_to_cart(self):
+        products_to_cart = []
+
+        if self.product_to_buy:
+            products_to_cart.append(self.product_to_buy)
+            return self.products_to_cart
